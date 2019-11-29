999499a4d9ad8f0ae880c2b30e1f9d872f23183a
Fran <francisco.pastor@colba.es>
2019-11-30 00:37:20 +0100

    Added Character Screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { CharacterScreen } from './screens/characterScreen';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <CharacterScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App
