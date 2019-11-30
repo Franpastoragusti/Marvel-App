065fc0d2816168656473a212067b60d7c2c37cd3
Fran <francisco.pastor@colba.es>
2019-11-30 14:31:50 +0100

    moved to header component

---
+++ components/header.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { StyleSheet, View, Image, Animated } from 'react-native';
+import Layout from "../constants/layout"
+export const Header = ({ scrollOffset }) => {
+    return (
+        <View style={styles.header}>
+            <Image style={styles.headerImage} source={require('../assets/images/marvelIcon.png')}></Image>
+            <Animated.View style={[styles.textContainer, {
+                height: scrollOffset.interpolate({
+                    inputRange: [0, 200],
+                    outputRange: [60, 30],
+                    extrapolate: 'clamp',
+                })
+            }]}>
+                <Animated.Text
+                    style={[styles.headerText, {
+                        fontSize: scrollOffset.interpolate({
+                            inputRange: [0, 200],
+                            outputRange: [30, 20],
+                            extrapolate: 'clamp',
+                        })
+                    }]}
+                >
+                    CHARACTERS
+                </Animated.Text>
+            </Animated.View>
+        </View>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+    header: {
+        paddingTop: 30,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "red",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 8,
+    },
+    headerImage: {
+        height: 50,
+        width: 100,
+        resizeMode: "contain"
+    },
+    headerText: {
+        fontSize: 30,
+        color: "#eeebfa",
+        fontFamily: 'space-mono',
+    },
+    textContainer: {
+        justifyContent: 'center',
+        alignItems: "center",
+        paddingHorizontal: Layout.window.width * 0.05,
+        width: Layout.window.width,
+        height: 60,
+    }
+});
