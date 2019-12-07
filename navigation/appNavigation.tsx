c8db3f5453f35f37ae3cb33d9d94abe8b434b4ca
Fran <francisco.pastor@colba.es>
2019-12-07 16:18:52 +0100

    created tab navigator

--- navigation/appNavigation.tsx
+++ navigation/appNavigation.tsx
@@ -1,11 +1,55 @@
+import React from "react"
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator } from "react-navigation-stack"
 import { CharacterScreen } from "../screens/characterScreen"
 import { CharacterDetailScreen } from "../screens/characterDetailScreen"
+import { ComicScreen } from '../screens/comicScreen';
+import { ComicDetailScreen } from '../screens/comicDetailScreen';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { Platform } from "react-native";
 
-export default createAppContainer(
-    createStackNavigator({
-        CharacterScreen: { screen: CharacterScreen },
-        CharacterDetail: { screen: CharacterDetailScreen },
-    }, { headerLayoutPreset: 'center' })
-);
+
+const CharacterStack = createStackNavigator({
+    CharaterScreen: CharacterScreen,
+    CharaterDetails: CharacterDetailScreen,
+});
+
+const ComicStack = createStackNavigator({
+    ComicScreen: ComicScreen,
+    ComicDetails: ComicDetailScreen,
+});
+
+
+export default createAppContainer(createBottomTabNavigator(
+    {
+        CharacterScreen: CharacterStack,
+        ComicScreen: ComicStack,
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                let IconComponent = Ionicons;
+                let iconName;
+                if (routeName === 'CharacterScreen') {
+                    iconName = Platform.OS === 'ios'
+                        ? `ios-people`
+                        : `md-people`
+                } else if (routeName === 'ComicScreen') {
+                    iconName = Platform.OS === 'ios'
+                        ? `ios-book`
+                        : `md-book`
+                }
+
+                return <IconComponent name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeBackgroundColor: "red",
+            inactiveBackgroundColor: "white",
+            activeTintColor: 'white',
+            inactiveTintColor: 'red',
+        },
+    }
+));
