5ff9fd1eee7ff9b4dfcc56fd685748482a1edbc2
Fran <francisco.pastor@colba.es>
2020-01-20 20:26:23 +0100

    added marvelDataSource

---
+++ services/marvelDataSource.tsx
@@ -0,0 +1,19 @@
+import CryptoJS from "crypto-js"
+import { IMarvelResponse } from "../types"
+const PUBLIC_KEY = "*****************************"
+const PRIVATE_KEY = "****************************"
+const BASE_URL = "http://gateway.marvel.com/v1/public"
+
+interface IMarvelProps {
+    pathName: string
+}
+export const MarvelDataSource = ({ pathName }: IMarvelProps): Promise<IMarvelResponse> => {
+    const ts = Date.now()
+    const hash = CryptoJS.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
+    var url = `${BASE_URL}/${pathName}?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(jsonResponse => jsonResponse.data)
+        .catch(e => console.warn(e))
+}
